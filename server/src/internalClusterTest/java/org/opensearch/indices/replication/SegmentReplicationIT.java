029a2f6feb5120c887049941dca2a29a531cdbb3
Marc Handalian <handalm@amazon.com>
2023-01-20 15:46:38 -0800

    mute
    Signed-off-by: Marc Handalian <handalm@amazon.com>

--- server/src/internalClusterTest/java/org/opensearch/indices/replication/SegmentReplicationIT.java
+++ server/src/internalClusterTest/java/org/opensearch/indices/replication/SegmentReplicationIT.java
@@ -422,6 +422,7 @@ public class SegmentReplicationIT extends SegmentReplicationBaseIT {
         assertDocCounts(docCount, primaryNode);
     }
 
+    @AwaitsFix(bugUrl = "https://github.com/opensearch-project/OpenSearch/issues/5669")
     public void testStartReplicaAfterPrimaryIndexesDocs() throws Exception {
         final String primaryNode = internalCluster().startNode();
         createIndex(INDEX_NAME, Settings.builder().put(indexSettings()).put(IndexMetadata.SETTING_NUMBER_OF_REPLICAS, 0).build());
