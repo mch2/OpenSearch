3c398d7105ab08465d78e03aa16109d248c6d4b4
Marc Handalian <marc.handalian@gmail.com>
2024-02-17 14:02:02 -0800

    just rip it out
    Signed-off-by: Marc Handalian <marc.handalian@gmail.com>

--- plugins/repository-s3/src/main/java/org/opensearch/repositories/s3/SocketAccess.java
+++ plugins/repository-s3/src/main/java/org/opensearch/repositories/s3/SocketAccess.java
@@ -38,7 +38,6 @@ import java.io.IOException;
 import java.net.SocketPermission;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
-import java.security.PrivilegedActionException;
 import java.security.PrivilegedExceptionAction;
 
 /**
@@ -52,25 +51,23 @@ public final class SocketAccess {
     private SocketAccess() {}
 
     public static <T> T doPrivileged(PrivilegedAction<T> operation) {
-        SpecialPermission.check();
-        return AccessController.doPrivileged(operation);
+        try {
+            return operation.run();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static <T> T doPrivilegedIOException(PrivilegedExceptionAction<T> operation) throws IOException {
-        SpecialPermission.check();
         try {
-            return AccessController.doPrivileged(operation);
-        } catch (PrivilegedActionException e) {
-            throw (IOException) e.getCause();
+            return operation.run();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 
     public static void doPrivilegedVoid(Runnable action) {
-        SpecialPermission.check();
-        AccessController.doPrivileged((PrivilegedAction<Void>) () -> {
-            action.run();
-            return null;
-        });
+        action.run();
     }
 
 }
